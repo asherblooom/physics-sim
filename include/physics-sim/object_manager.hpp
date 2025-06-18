3b2bc0caa9153c94f6c3c752900d8116fd0003d2
Asher Bloom <si23870@bristol.ac.uk>
2025-06-18 19:03:10 +0100

    make object manager class nicer

--- include/physics-sim/object_manager.hpp
+++ include/physics-sim/object_manager.hpp
@@ -16,22 +16,13 @@ private:
 public:
 	template <typename Obj>
 		requires std::is_base_of_v<PhysObject, Obj>
-	static std::shared_ptr<PhysObject> addObject(std::string name, glm::vec2 pos, glm::vec3 color, glm::vec2 velocity) {
-		std::shared_ptr<PhysObject> object = std::make_shared<Obj>(pos, color, velocity);
-		Objects[name] = object;
-		return object;
-	}
-	static void deleteObject(std::string name) {
-		Objects.erase(name);
-	}
-	static std::shared_ptr<PhysObject> getObject(std::string name) {
-		return Objects[name];
-	}
-	static void renderObjects(SpriteRenderer& renderer) {
-		for (auto object : Objects) {
-			object.second->Draw(renderer);
-		}
-	}
+	static std::shared_ptr<PhysObject> addObject(std::string name, glm::vec2 pos, glm::vec3 color, glm::vec2 velocity);
+
+	static void deleteObject(std::string name);
+
+	static std::shared_ptr<PhysObject> getObject(std::string name);
+
+	static void renderObjects(SpriteRenderer& renderer);
 };
 
 #endif
--- src/object_manager.cpp
+++ src/object_manager.cpp
@@ -1,2 +1,21 @@
 #include <physics-sim/object_manager.hpp>
 std::map<std::string, std::shared_ptr<PhysObject>> ObjectManager::Objects;
+
+template <typename Obj>
+	requires std::is_base_of_v<PhysObject, Obj>
+std::shared_ptr<PhysObject> ObjectManager::addObject(std::string name, glm::vec2 pos, glm::vec3 color, glm::vec2 velocity) {
+	std::shared_ptr<PhysObject> object = std::make_shared<Obj>(pos, color, velocity);
+	Objects[name] = object;
+	return object;
+}
+void ObjectManager::deleteObject(std::string name) {
+	Objects.erase(name);
+}
+std::shared_ptr<PhysObject> ObjectManager::getObject(std::string name) {
+	return Objects[name];
+}
+void ObjectManager::renderObjects(SpriteRenderer& renderer) {
+	for (auto object : Objects) {
+		object.second->Draw(renderer);
+	}
+}
